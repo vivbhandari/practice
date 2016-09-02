9374e427883fec21fa8294c647d5554b8c461fef
Vivek Bhandari <vbhandari@vbhandari-mbpr.local>
2016-09-01 17:37:32 -0700

    algorith practice

---
+++ src/algorithms/LargestSumContigous.java
@@ -0,0 +1,23 @@
+package algorithms;
+
+public class LargestSumContigous {
+
+	public static int getLargestSumContiguous(int[] input) {
+		int maxSum = 0;
+		int currentSum = 0;
+
+		for (int i : input) {
+			currentSum += i;
+			if (currentSum < 0)
+				currentSum = 0;
+			if (maxSum < currentSum)
+				maxSum = currentSum;
+		}
+		return maxSum;
+	}
+
+	public static void main(String args[]) {
+		int[] input = new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+		System.out.println(getLargestSumContiguous(input));
+	}
+}
