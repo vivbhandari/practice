4a4fba9da38ee8096dfdf187529aa839a2e78cab
vivbhandari <bhandari.viv@gmail.com>
2018-03-13 00:37:08 -0700

    linked list reverse

--- src/linkedlist/TestLinkedList.java
+++ src/linkedlist/TestLinkedList.java
@@ -4,6 +4,32 @@ public class TestLinkedList {
 
 	static int counter = 0;
 
+	private static LinkedNode reverse(LinkedNode cur, LinkedNode prev) {
+		LinkedNode newHead = null;
+		if (cur.nextNode == null) {
+			newHead = cur;
+		} else {
+			newHead = reverse(cur.nextNode, cur);
+		}
+		cur.nextNode = prev;
+		return newHead;
+	}
+
+	private static LinkedNode reverseItr(LinkedNode head) {
+		LinkedNode dummy = new LinkedNode(-1) ;
+		dummy.nextNode = head;
+		LinkedNode move = head.nextNode;
+		
+		while(move != null ){
+			head.nextNode = move.nextNode;
+			move.nextNode = dummy.nextNode;
+			dummy.nextNode = move;
+			move = head.nextNode;
+		}
+		
+		return dummy.nextNode;
+	}
+
 	private static LinkedNode recurse(LinkedNode currentNode, int n) {
 		LinkedNode targetNode = null;
 		if (currentNode != null) {
@@ -46,26 +72,28 @@ public class TestLinkedList {
 		}
 
 		System.out.println(linkedList);
-		System.out.println(linkedList.getLenght());
-		System.out.println(linkedList.get(3));
-		linkedList.delete(linkedList.get(3));
-		System.out.println(linkedList);
-		linkedList.delete(linkedList.get(0));
-		System.out.println(linkedList);
-		linkedList.add(new LinkedNode(3), 2);
-		System.out.println(linkedList);
-		linkedList.add(new LinkedNode(5), 7);
-		System.out.println(linkedList);
-		linkedList.add(new LinkedNode(0), 0);
-		System.out.println(linkedList);
-		System.out.println(getFromEnd(linkedList, 1));
-		System.out.println(getFromEnd(linkedList, 3));
-		System.out.println(getFromEnd(linkedList, 6));
-		System.out.println(getFromEnd(linkedList, 8));
-
-		System.out.println(getFromEndIterative(linkedList, 1));
-		System.out.println(getFromEndIterative(linkedList, 3));
-		System.out.println(getFromEndIterative(linkedList, 6));
-		System.out.println(getFromEndIterative(linkedList, 8));
+//		System.out.println(reverse(linkedList.head, null));
+		System.out.println(reverseItr(linkedList.head));
+//		System.out.println(linkedList.getLenght());
+//		System.out.println(linkedList.get(3));
+//		linkedList.delete(linkedList.get(3));
+//		System.out.println(linkedList);
+//		linkedList.delete(linkedList.get(0));
+//		System.out.println(linkedList);
+//		linkedList.add(new LinkedNode(3), 2);
+//		System.out.println(linkedList);
+//		linkedList.add(new LinkedNode(5), 7);
+//		System.out.println(linkedList);
+//		linkedList.add(new LinkedNode(0), 0);
+//		System.out.println(linkedList);
+//		System.out.println(getFromEnd(linkedList, 1));
+//		System.out.println(getFromEnd(linkedList, 3));
+//		System.out.println(getFromEnd(linkedList, 6));
+//		System.out.println(getFromEnd(linkedList, 8));
+//
+//		System.out.println(getFromEndIterative(linkedList, 1));
+//		System.out.println(getFromEndIterative(linkedList, 3));
+//		System.out.println(getFromEndIterative(linkedList, 6));
+//		System.out.println(getFromEndIterative(linkedList, 8));
 	}
 }
